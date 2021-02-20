c9f62430956109a80c1643fb3c3b9f5d3419428c
enhuichang <enhuichang@gmail.com>
2021-02-20 17:04:17 +0800

    fxied

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,5 +1,15 @@
-func greeting2(whoToGreet:String){
-    print("Hello\(whoToGreet)")
+func loveCalculator(){
+    let loveScore=Int.random(in: 0...100)
+    
+    switch(loveScore){
+    case 80...100:  print("YOu love each other like kanye loves kanye.")
+    case 41..<81: print("You go together like coke and mentos")
+    case ...40: print("you'll be forever alone")
+    default: print("Error lovescore is out of range.")
+    
+    }
+
+
 }
 
-greeting2(whoToGreet: "Gary")
+loveCalculator()
