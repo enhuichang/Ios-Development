a15c65849bf69af4ce3b2d740eaba0f3bbd8a07b
enhuichang <enhuichang@gmail.com>
2021-02-20 22:35:52 +0800

    fixed

--- EggTimer-iOS13/EggTimer/ViewController.swift
+++ EggTimer-iOS13/EggTimer/ViewController.swift
@@ -7,13 +7,69 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-    
-    let eggTime:[String:Int]=["Soft":5,"Medium":7,"Hard":12]
+    var player: AVAudioPlayer?
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    let eggTime:[String:Int]=["Soft":3 ,"Medium":5,"Hard":7]
+    var timer = Timer()
+
+    var secondPassed = 0
+    var totalTime = 0
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        let hardness=sender.currentTitle
-        print(eggTime[hardness!])
+        
+        timer.invalidate()
+        let hardness=sender.currentTitle!
+        totalTime=eggTime[hardness]!
+
+        progressBar.progress=0.0
+        secondPassed = 0
+        
+        titleLabel.text=hardness
+
+        
+        timer=Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+
+    @objc func updateTimer() {
+        if(secondPassed < totalTime) {
+            let precentageProgress=Float(secondPassed)/Float(totalTime)
+            
+            progressBar.progress=precentageProgress
+            secondPassed+=1
+        }
+        else{
+            timer.invalidate()
+            titleLabel.text="DONE!"
+            progressBar.progress=1.0
+            playSound(soundName: "alarm_sound")
+        }
+
+    }
+    
+    func playSound(soundName:String) {
+        guard let url = Bundle.main.url(forResource: soundName, withExtension: "mp3") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            /* iOS 10 and earlier require the following line:
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
     }
 }
--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,15 +1,5 @@
-func loveCalculator(){
-    let loveScore=Int.random(in: 0...100)
-    
-    switch(loveScore){
-    case 80...100:  print("YOu love each other like kanye loves kanye.")
-    case 41..<81: print("You go together like coke and mentos")
-    case ...40: print("you'll be forever alone")
-    default: print("Error lovescore is out of range.")
-    
-    }
+var player1Usernamer:String?=nil
 
+//player1Usernamer="jack"
 
-}
-
-loveCalculator()
+print(player1Usernamer ?? "China")
