90715f22ccb7155a3cb00b3cc34a3235e48b3c97
enhuichang <enhuichang@gmail.com>
2021-02-21 17:12:29 +0800

    fixed

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,5 +1,24 @@
-var player1Usernamer:String?=nil
+struct Town{
+    let name:String
+    var citizens:[String]
+    var resouce:[String:Int]
+    
+    func fortify(){
+        print("Defences increased!")
+    }
+    
+    init (name:String,citizens:[String],resouce :[String:Int]){
+        self.name=name
+        self.citizens=citizens
+        self.resouce=resouce
+    }
+}
 
-//player1Usernamer="jack"
+var anotherTown=Town(name: "Nameless Island", citizens: ["Tom hanks"], resouce: ["Cocunuts" : 100])
 
-print(player1Usernamer ?? "China")
+
+var ghostTown=Town(name: "Ghosty Mcghostface", citizens: [], resouce: ["Tumbleweed" : 1])
+
+
+anotherTown.citizens.append("Wilson")
+ghostTown.fortify()
