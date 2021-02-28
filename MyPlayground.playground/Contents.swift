218dcd66d875a28d0c539b612f63b3f4f390eace
enhuichang <enhuichang@gmail.com>
2021-02-28 16:21:08 +0800

    fixed

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,24 +1,9 @@
-struct Town{
-    let name:String
-    var citizens:[String]
-    var resouce:[String:Int]
-    
-    func fortify(){
-        print("Defences increased!")
-    }
-    
-    init (name:String,citizens:[String],resouce :[String:Int]){
-        self.name=name
-        self.citizens=citizens
-        self.resouce=resouce
-    }
-}
 
-var anotherTown=Town(name: "Nameless Island", citizens: ["Tom hanks"], resouce: ["Cocunuts" : 100])
+import UIKit
+
+let array=[6,2,3,9,4,1]
 
 
-var ghostTown=Town(name: "Ghosty Mcghostface", citizens: [], resouce: ["Tumbleweed" : 1])
+let newArray=array.map({"\($0)"})
 
-
-anotherTown.citizens.append("Wilson")
-ghostTown.fortify()
+print(newArray)
