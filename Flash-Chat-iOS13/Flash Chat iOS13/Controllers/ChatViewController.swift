df2fbc6fb43a93a17433578f8e9fd778f936d56a
enhuichang <enhuichang@gmail.com>
2021-03-11 14:58:52 +0800

    fixed

--- Flash-Chat-iOS13/Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash-Chat-iOS13/Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -15,34 +15,52 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var messageTextfield: UITextField!
     
     let db = Firestore.firestore()
-<<<<<<< Updated upstream
-=======
-    
-    
->>>>>>> Stashed changes
-    var messages:[Message]=[
-        Message(sender: "1@2.com", body: "Hey!"),
-        Message(sender: "1@b.com", body: "Hello!"),
-        Message(sender: "1@2.com", body: "What's up?第五代；问；觉得；额外；IE我；额外；为二位；一；额；i"),
-    ]
+    var messages:[Message]=[]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource=self
         title=K.appName
         navigationItem.hidesBackButton=true
-        
-        tableView.register(UINib(nibName: K.cellNibName,bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
+        loadMessages()
+    }
+    func loadMessages(){
+        db.collection(K.FStore.collectionName)
+            .order(by: K.FStore.dateField)
+            .addSnapshotListener { (querySnapShot, error) in
+            self.messages = []
+            if let e = error{
+                print("There was an issue retrieving data from FireStore. \(e)")
+            }else{
+                if let snapshotDocuments = querySnapShot?.documents{
+                    for doc in snapshotDocuments{
+                        let data = doc.data()
+                         if let sender = data [K.FStore.senderField] as? String,
+                            let messageBody = data[K.FStore.bodyField] as? String{
+                            let newMessage = Message(sender: sender, body: messageBody)
+                            self.messages.append(newMessage)
+                            
+                            DispatchQueue.main.async {
+                                self.tableView.reloadData()
+                            }
+                         }
+                    }
+                    
+                
+                }
+            }
+        }
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
-<<<<<<< Updated upstream
         if let messageBody = messageTextfield.text, let messageSender =
             Auth.auth().currentUser?.email{
             
             db.collection(K.FStore.collectionName).addDocument(data: [
             K.FStore.senderField: messageSender,
-                K.FStore.bodyField: messageBody
+                K.FStore.bodyField: messageBody,
+                K.FStore.dateField: Date().timeIntervalSince1970
             ]){
                 (error)in if let e = error{
                     print("There was an issue saving date to fireStore, \(e)")
@@ -50,10 +68,6 @@ class ChatViewController: UIViewController {
                     print("Successfully saveda data.")
                 }
             }
-=======
-        if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email{
-            db.collection(K.FStore.collectionName).addDocument(data: <#T##[String : Any]#>)
->>>>>>> Stashed changes
         }
     }
     
@@ -75,9 +89,10 @@ extension ChatViewController:UITableViewDataSource{
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell=tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier,for: indexPath)
-        as! MessageCell
-        cell.label?.text=messages[indexPath.row].body
+        let message = messages[indexPath.row]
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        cell.label?.text = message.body
         return cell
     }
     
--- Flash-Chat-iOS13/Flash Chat iOS13/Views/MessageCell.swift
+++ Flash-Chat-iOS13/Flash Chat iOS13/Views/MessageCell.swift
@@ -14,10 +14,12 @@ class MessageCell: UITableViewCell {
     @IBOutlet weak var label: UILabel!
     
     @IBOutlet weak var rightImageView: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-        messageBubble.layer.cornerRadius = messageBubble.frame.size.height/5
+        messageBubble.layer.cornerRadius = messageBubble.frame.size.height / 5
+
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
