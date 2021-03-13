2ec0e9986f9a42cc76d2274a10b4632135a3ff70
enhuichang <enhuichang@gmail.com>
2021-03-13 20:22:05 +0800

    fixed

--- AngelaCard/AngelaCard/ContentView.swift
+++ AngelaCard/AngelaCard/ContentView.swift
@@ -10,11 +10,20 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack{
-            Color(red:0.09,green:0.63,blue:0.52).edgesIgnoringSafeArea(    .all)
-            Text("Hello, world!")
-                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                .bold().foregroundColor(.white)
-            .padding()
+            Color(red:0.09,green:0.63,blue:0.52)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("Angela").resizable().aspectRatio(contentMode: .fit)
+                    .frame(width: 150, height: 150, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/).overlay(Circle().stroke(Color.white,lineWidth: 5))
+                Text("Angela Yu")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                    .padding()
+                Text("Ios Developer").foregroundColor(.white)
+                    .font(.system(size: 25))
+            }
         }
     }
 }
